4fcb242d229d94696a87a26f9fb9cd92baeae34b
trogdoro <craig.muth@gmail.com>
2011-08-23 01:49:43 -0700

    Added Spit.s for displaying source info (file, method, and line numben).

--- lib/spit.rb
+++ lib/spit.rb
@@ -1,22 +1,46 @@
 def Spit txt
-  line = caller[0]
+  Spit.print txt, caller[0]
+end
 
-  txt = txt.inspect rescue txt
+module Spit
+  def self.s txt
+    self.print txt, caller[0], :source=>true
+  end
 
-  path, number = line.match(/(.+):(\d+)/)[1..2]
+  def self.print txt, line, options={}
 
-  label = IO.readlines("/projects/homerun/homerun/app/controllers/web/dev_controller.rb")[number.to_i - 1]
-  label.strip!.sub!(/Ol /, '')
+    txt = txt.inspect rescue txt
 
-  clear, green, red, yellow, blue, pink, italic, bold, highlight = "\e[0m", "\e[32m", "\e[31m", "\e[33m", "\e[36m", "\e[35m", "\e[3m", "\e[1m", "\e[7m"
-  puts "#{green}#{italic}#{label}:#{clear}#{bold}#{yellow}  #{txt}#{clear}"
+    path, number = line.match(/(.+):(\d+)/)[1..2]
 
-  #   clear, green, red, yellow, blue, pink, gray, italic, bold = "\e[0m", "\e[32m", "\e[31m", "\e[33m", "\e[36m", "\e[35m", "\e[164m", "\e[3m", "\e[1m"
-  #   puts "#{red}#{txt}#{clear}"
-  #   puts "#{green}#{label}:#{clear}  #{green}#{highlight}#{txt}#{clear}"
-  #   puts "#{green}#{label}:#{clear}#{highlight}#{green}  #{txt}#{clear}"
-  #   puts "#{gray}#{txt}:#{clear}#{yellow} #{txt}#{clear}"
-  #   puts "#{red}#{txt}:#{clear}#{yellow} #{txt}#{clear}"
-  #   puts "#{red}#{txt} #{clear} aa"
-  #   puts "#{green}#{label}:#{clear} #{txt}\naaa"
+    label = IO.readlines("/projects/homerun/homerun/app/controllers/web/dev_controller.rb")[number.to_i - 1]
+    label.strip!.sub!(/Spit\S+ /, '')
+
+    label = "#{Spit.extract_label(Spit.parse_line line)} #{label}" if options[:source]
+
+    clear, green, red, yellow, blue, pink, italic, bold, highlight = "\e[0m", "\e[32m", "\e[31m", "\e[33m", "\e[36m", "\e[35m", "\e[3m", "\e[1m", "\e[7m"
+    puts "#{green}#{italic}#{label}:#{clear}#{bold}#{yellow}  #{txt}#{clear}"
+
+    #   clear, green, red, yellow, blue, pink, gray, italic, bold = "\e[0m", "\e[32m", "\e[31m", "\e[33m", "\e[36m", "\e[35m", "\e[164m", "\e[3m", "\e[1m"
+    #   puts "#{red}#{txt}#{clear}"
+    #   puts "#{green}#{label}:#{clear}  #{green}#{highlight}#{txt}#{clear}"
+    #   puts "#{green}#{label}:#{clear}#{highlight}#{green}  #{txt}#{clear}"
+    #   puts "#{gray}#{txt}:#{clear}#{yellow} #{txt}#{clear}"
+    #   puts "#{red}#{txt}:#{clear}#{yellow} #{txt}#{clear}"
+    #   puts "#{red}#{txt} #{clear} aa"
+    #   puts "#{green}#{label}:#{clear} #{txt}\naaa"
+
+  end
+
+  def self.parse_line path
+    method = path[/`(.+)'/, 1]   # `
+    path, l = path.match(/(.+):(\d+)/)[1..2]
+    path = File.expand_path path
+    clazz = path[/.+\/(.+)\.rb/, 1]
+    {:path=>path, :line=>l, :method=>method, :clazz=>clazz}
+  end
+
+  def self.extract_label h
+    "#{h[:clazz]}.#{h[:method]}(#{h[:line]})"
+  end
 end
